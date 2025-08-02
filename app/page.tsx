ecc6e383cebf736c463f0a46920932c590b7d8ff
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-02 15:58:30 +0530

    feat:learnt about the card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 
 import Areachart from '@/components/Areachart'
+import Cardlist from '@/components/Cardlist'
 import Chart from '@/components/Chart'
 import React from 'react'
 
@@ -7,11 +8,11 @@ function page() {
   return (
     <div className='grid grid-cols-1 lg:grid-col-2 2xl:grid-cols-4 gap-4'>
       <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2'><Chart /></div>
-      <div className='bg-primary-foreground  p-4 rounded-lg'>test2</div>
+      <div className='bg-primary-foreground  p-4 rounded-lg'><Cardlist title='Popular Content'/></div>
       <div className='bg-primary-foreground  p-4 rounded-lg'>test3</div>
       <div className='bg-primary-foreground  p-4 rounded-lg'>test4</div>
       <div className='bg-primary-foreground  p-4 rounded-lg lg:col-span-2'><Areachart /></div>
-      <div className='bg-primary-foreground  p-4 rounded-lg'>test6</div>
+      <div className='bg-primary-foreground  p-4 rounded-lg'><Cardlist title='Transactions'/></div>
      
       
      
